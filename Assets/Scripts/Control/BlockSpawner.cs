807a8f73056e1a4f751866bd023519446b379813
jarikki <jarikkigg@gmail.com>
2024-07-08 17:40:09 +0900

    Revert "2024-07-08"
    This reverts commit 30097c6f60b0e6ee742051dc44b506b59464182f.

--- Assets/Scripts/Control/BlockSpawner.cs
+++ Assets/Scripts/Control/BlockSpawner.cs
@@ -9,28 +9,23 @@ public class BlockSpawner : MonoBehaviour
     //블럭 프리팹 참조 변수
     public GameObject[] blockPrefab;
     //블럭 인스턴스 참조 변수
-    public GameObject activatedBlock;
+    private GameObject block;
     //블럭 인덱스
     private int blockIdx = 0;
     //블럭 생성 x 좌표
     private float positionX = 0.0f;
-    //블럭 생성 가능 여부
-    public bool canSpawn = true;
+    // Start is called before the first frame update
     void Start()
     {
-
+        //블럭 생성 메서드 호출 !!!수정 예정!!!
+        InvokeRepeating("SpawnBlock", 1.0f, 1.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (canSpawn)
-        {
-            canSpawn = false;
-            SpawnBlock();    
-        }
+        
     }
-
     //블럭 생성 메서드
     void SpawnBlock () 
     {
@@ -41,7 +36,7 @@ public class BlockSpawner : MonoBehaviour
             case 5:
                 positionX = Random.Range(0, 9);
                 break;
-            case 6:
+                case 6:
                 positionX = Random.Range(0, 10);
                 break;
             default:
@@ -49,9 +44,8 @@ public class BlockSpawner : MonoBehaviour
                 break;
         }
         //블럭 생성 후 참조
-        activatedBlock = Instantiate(blockPrefab[blockIdx]);
-        Debug.Log("블럭 생성");
+        block = Instantiate(blockPrefab[blockIdx]);
         //블럭 초기 위치 설정
-        activatedBlock.transform.position = new Vector3(positionX, blockPrefab[blockIdx].transform.position.y);
+        block.transform.position = new Vector3(positionX, blockPrefab[blockIdx].transform.position.y);
     }
 }
